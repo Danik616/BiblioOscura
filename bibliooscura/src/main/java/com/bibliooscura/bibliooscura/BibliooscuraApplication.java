40de85fb4cb57b690fd9c10974eccae2ddab797f
Danik616 <ddanielvargas2011@gmail.com>
2023-04-03 09:52:11 -0500

    Se hace la creación incial del proyecto

---
+++ bibliooscura/src/main/java/com/bibliooscura/bibliooscura/BibliooscuraApplication.java
@@ -0,0 +1,13 @@
+package com.bibliooscura.bibliooscura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliooscuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliooscuraApplication.class, args);
+	}
+
+}
